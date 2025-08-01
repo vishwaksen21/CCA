b39393eea85a4ce17f63638a0248c2934b316506
hulk buster <hulkbuster646@gmail.com>
2025-08-01 20:46:13 +0000

    Error: A tree hydrated but some attributes of the server rendered HTML d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,11 +48,11 @@ export default function RootLayout({
           <AnimatePresence mode="wait">
             <motion.main
               key={pathname}
-              initial={{ opacity: 0, y: 15 }}
+              initial={{ opacity: 0, y: 10 }}
               animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -15 }}
+              exit={{ opacity: 0, y: -10 }}
               transition={{
-                duration: 0.4,
+                duration: 0.3,
                 ease: 'easeInOut',
               }}
               className="flex-1"
